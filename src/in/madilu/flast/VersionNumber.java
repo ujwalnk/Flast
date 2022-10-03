38e4a2c1293e939f233c889c43d382103ff28bf6
Ujwal N K <ujwal.nk@gmail.com>
2022-10-03 19:29:36 +0530

    Update Version Number

--- src/in/madilu/flast/VersionNumber.java
+++ src/in/madilu/flast/VersionNumber.java
@@ -2,7 +2,7 @@ package in.madilu.flast;
 
 public class VersionNumber{
     // App Version Number
-    public static final String VERSION = "b0.2.2";
+    public static final String VERSION = "b0.2.3";
 
     private VersionNumber(){}
 }
